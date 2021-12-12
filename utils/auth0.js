ec109fda0398ccb9ef80237a997827b3316fa338
ripanionut <43762799+ripanionut@users.noreply.github.com>
2021-12-12 15:09:54 +0200

    Update auth0.js

--- utils/auth0.js
+++ utils/auth0.js
@@ -1,24 +1,5 @@
 import { getSession } from "@auth0/nextjs-auth0";
-import { initAuth0 } from "@auth0/nextjs-auth0";
 
-// const auth0 = initAuth0({
-//   secret: process.env.AUTH0_SECRET,
-//   baseURL: process.env.AUTH0_BASE_URL,
-//   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URLl,
-//   clientID: process.env.AUTH0_CLIENT_ID,
-//   clientSecret: process.env.AUTH0_CLIENT_SECRET,
-// });
-
-const auth0 = initAuth0({
-  secret: process.env.AUTH0_SECRET,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-  baseURL: process.env.AUTH0_BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  routes: {
-      postLogoutRedirect: process.env.AUTH0_POST_LOGOUT_REDIRECT_URI
-  }
-});
 export const isAuthorized = (user, role) => {
   return user && user["http://portfolio-ripan.com/roles"].includes(role);
 };
