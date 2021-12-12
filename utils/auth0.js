fb6a17d86eae4e96b071c1ea02a934617d362f7a
ripanionut <ripan_ionut@yahoo.com>
2021-12-12 12:04:54 +0200

    first committ

--- utils/auth0.js
+++ utils/auth0.js
@@ -4,7 +4,7 @@ import { initAuth0 } from "@auth0/nextjs-auth0";
 // const auth0 = initAuth0({
 //   secret: process.env.AUTH0_SECRET,
 //   baseURL: process.env.AUTH0_BASE_URL,
-//   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+//   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URLl,
 //   clientID: process.env.AUTH0_CLIENT_ID,
 //   clientSecret: process.env.AUTH0_CLIENT_SECRET,
 // });
